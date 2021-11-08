752e79fdb8db4a53b75518048b4d6d9a4acffa5f
Vera Juan <verajuan7@hotmail.com>
2021-11-08 16:47:51 -0300

    rk y euler terminado

--- SIMULACION-TP1/EcuacionDiferencial.cs
+++ SIMULACION-TP1/EcuacionDiferencial.cs
@@ -22,7 +22,7 @@ namespace SIMULACION_TP1
         double rk_l4 { get; set; }
 
         //EULER
-        double x1 { get; set; }
+        double eu_x1 { get; set; }
         double dx1 { get; set; }
         double dx2 { get; set; }
 
@@ -41,6 +41,10 @@ namespace SIMULACION_TP1
                 //RUNGE KUTTA
                 rk_x1 = x1;
                 rk_x2 = x2;
+
+                //EULER
+                eu_x1 = x1;
+                dx1 = x2;
             }
             else
             {
@@ -49,11 +53,27 @@ namespace SIMULACION_TP1
                 //RUNGE KUTTA
                 rk_x1 = ecuacionAnterior.rk_x1 + (ecuacionAnterior.rk_k1 + 2* ecuacionAnterior.rk_k2 + 2 * ecuacionAnterior.rk_k3 + ecuacionAnterior.rk_k4) / 6;
                 rk_x2 = ecuacionAnterior.rk_x2 + (ecuacionAnterior.rk_l1 + 2* ecuacionAnterior.rk_l2 + 2 * ecuacionAnterior.rk_l3 + ecuacionAnterior.rk_l4) / 6;
+
+                //EULER
+                eu_x1 = ecuacionAnterior.eu_x1 + h * ecuacionAnterior.dx1;
+                dx1 = ecuacionAnterior.dx1 + h * ecuacionAnterior.dx2;
             }
 
             //RUNGE KUTTA
-            //rk_l1 = h * (Math.Exp())
+            rk_l1 = h * ((Math.Exp(-c * tn)) - (a * rk_x2) - (b * rk_x1));
+            rk_k1 = h * rk_x2;
 
+            rk_l2 = h * ((Math.Exp(-c * (tn + 0.5 * h))) - (a * (rk_x2 + 0.5 * rk_l1)) - (b * (rk_x1 + 0.5 * rk_k1)));
+            rk_k2 = h * (rk_x2 + 0.5 * rk_l1);
+
+            rk_l3 = h * ((Math.Exp(-c * (tn + 0.5 * h))) - (a * (rk_x2 + 0.5 * rk_l2)) - (b * (rk_x1 + 0.5 * rk_k2)));
+            rk_k3 = h * (rk_x2 + 0.5 * rk_l2);
+
+            rk_l4 = h * ((Math.Exp(-c * (tn + h))) - (a * (rk_x2 + rk_l3)) - (b * (rk_x1 + rk_k3)));
+            rk_k4 = h * (rk_x2 + 0.5 * rk_l3);
+
+            //EULER
+            dx2 = h * ((Math.Exp(-c * tn)) - (a * dx1) - (b * eu_x1));
 
         }
 
