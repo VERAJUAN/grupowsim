f2ff98d18915607d30c2e2fbece8ded9bde38c21
Martin Cuello <mcuello@evoltis.com>
2021-11-09 17:48:37 -0300

    titlulo nro tp menu

--- SIMULACION-TP1/Menu.Designer.cs
+++ SIMULACION-TP1/Menu.Designer.cs
@@ -52,7 +52,7 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(173, 40);
             this.button2.TabIndex = 2;
-            this.button2.Text = "Ejercicio TP5";
+            this.button2.Text = "Ejercicio TP6";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
@@ -64,7 +64,7 @@
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(162, 42);
             this.label2.TabIndex = 4;
-            this.label2.Text = "TP5 SIM";
+            this.label2.Text = "TP6 SIM";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // textBox1
